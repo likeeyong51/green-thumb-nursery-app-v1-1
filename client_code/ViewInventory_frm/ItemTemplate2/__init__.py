097770dc16d643af39f56e03ae8a24b2f356b5b1
likeeyong17@gmail.com <likeeyong17@gmail.com>
2025-08-13 00:35:51 +0000

    Edited ViewInventory_frm.ItemTemplate2

--- client_code/ViewInventory_frm/ItemTemplate2/__init__.py
+++ client_code/ViewInventory_frm/ItemTemplate2/__init__.py
@@ -26,10 +26,16 @@ class ItemTemplate2(ItemTemplate2Template):
         """This method is called when the button is clicked"""
         # enable the record fields
         if not self.name_txb.enabled:
-        self.name_txb.enabled      = \
-        self.type_drp.enabled      = \
-        self.price_txb.enabled     = \
-        self.stock_qty_txb.enabled = True
+            self.name_txb.enabled      = \
+            self.type_drp.enabled      = \
+            self.price_txb.enabled     = \
+            self.stock_qty_txb.enabled = True
+        else: # disable the record fields
+            self.name_txb.enabled      = \
+            self.type_drp.enabled      = \
+            self.price_txb.enabled     = \
+            self.stock_qty_txb.enabled = False
+            self.confirm_type_change_btn.visible = False
 
         # edit confirmation completed via change event of the fields
         
@@ -37,3 +43,16 @@ class ItemTemplate2(ItemTemplate2Template):
     def delete_btn_click(self, **event_args):
         """This method is called when the button is clicked"""
         pass
+
+    def plant_field_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in this text box"""
+        pass
+
+    def confirm_type_change_btn_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+    def type_drp_change(self, **event_args):
+        """This method is called when an item is selected"""
+        # show confirm button to apply change of palny type
+        self.confirm_type_change_btn.visible = True
